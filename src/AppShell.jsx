70155aedc4c4adb0bf08d6a580947b920fbe12f8
lloydcobb1991 <lloyd.cobb1991@gmail.com>
2026-06-02 15:17:23 -0700

    s

--- src/AppShell.jsx
+++ src/AppShell.jsx
@@ -557,7 +557,7 @@ function WelcomePanel() {
             lineHeight: 1.1,
           }}
         >
-          Ignite Creative Services
+          Ignite Creative Services Tool Belt
         </h1>
         <div
           style={{
